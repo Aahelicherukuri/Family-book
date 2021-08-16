5b5c74043c1d029ec2de4a7afa6228f05e7d6d40
Aahelicherukuri <88046468+Aahelicherukuri@users.noreply.github.com>
2021-08-16 22:31:03 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+var images =
+    https: //th.bing.com/th/id/OIP.Tcd9yqjSBDufSCCZmD1sGwHaD7?w=320&h=180&c=7&o=5&dpr=1.25&pid=1.7,https://th.bing.com/th/id/OIP.K6mhZlLqVPgz2qUytca7AwHaHa?w=199&h=199&c=7&o=5&dpr=1.25&pid=1.7,
+    https: //th.bing.com/th/id/OIP.FE4fQg2oLH6wrC7uoVWRxwHaLF?w=204&h=305&c=7&o=5&dpr=1.25&pid=1.7,https://th.bing.com/th/id/OIP.AUftiPwQ8cT-BpMnhyd5-QHaGS?w=255&h=216&c=7&o=5&dpr=1.25&pid=1.7,
+    https: //th.bing.com/th/id/OIP.G0rJkMYcJwyiiM28G5cHigHaEY?w=281&h=180&c=7&o=5&dpr=1.25&pid=1.7,https://th.bing.com/th/id/OIP.Y2TAR1-KNLpbdht8diLVBQHaIm?w=134&h=180&c=7&o=5&dpr=1.25&pid=1.7
+
+    var names = ["Family book", "sonia singi", "Ranbir singi", "allia singi", "rocky singi", "ria singi", "diavlit singi"];
